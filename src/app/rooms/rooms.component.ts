f5fd3228cc429169912d6e2f8a539db9754aa04a
gitab12 <geethabasavraju18@gmail.com>
2023-12-05 18:08:42 +0530

    commit

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,52 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy,Component, OnInit } from '@angular/core';
 import { Rooms, RoomsList } from './roomsinterface';
 import { CommonModule } from '@angular/common';
 import { RoomlistComponent } from './roomlist/roomlist.component';
+
 @Component({
   selector: 'app-rooms',
   standalone: true,
-  imports: [CommonModule,RoomlistComponent],
+  imports: [CommonModule, RoomlistComponent],
   templateUrl: './rooms.component.html',
-  styleUrl: './rooms.component.scss'
+  styleUrl: './rooms.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RoomsComponent implements OnInit{
-  
-  hotelname ="Hotel Inventory";
-  numberofrooms =10;
-  hiderooms =false;
-  
-  toggle(){
+export class RoomsComponent implements OnInit {
+
+  hotelname = "Hotel Inventory";
+  numberofrooms = 10;
+  hiderooms = false;
+
+  toggle() {
     this.hiderooms = !this.hiderooms
   }
-  SelectRoom(room:RoomsList)
-  {
+  addroom() {
+    const room: RoomsList = {
+      roomstype: 2,
+      checkintime: new Date(),
+      checkout: new Date(),
+      cost: 1300
+    }
+   //to add the new record with the existing record (to achive imutablity we use spread operator i.e ... 3dots)
+   // below code says that add the new record with and exixting record[...this.roomlist -add the exixting record,room-add the new record with exixting record]
+   this.roomlist = [...this.roomlist,room]
+   // this.roomlist.push(room);
+  }
+  SelectRoom(room: RoomsList) {
     console.log(room);
   }
-  room : Rooms=
-  {
-  availablerooms :10,
-  bookedrooms: 5,
-  totalrooms:20
-  }
-  
-  roomlist : RoomsList[] = [];
-  constructor() {}
+  room: Rooms =
+    {
+      availablerooms: 10,
+      bookedrooms: 5,
+      totalrooms: 20
+    }
+
+  roomlist: RoomsList[] = [];
+  constructor() { }
   ngOnInit(): void {
-   this.roomlist =[
-  
-  {
-    roomstype: 1,
-    checkintime: new Date(),
-    checkout:new Date(),
-    cost: 1200
-  },
-  {
-    roomstype: 2,
-    checkintime: new Date(),
-    checkout:new Date(),
-    cost: 1300
-  }
-]
-  
+    this.roomlist = [
+
+      {
+        roomstype: 1,
+        checkintime: new Date(),
+        checkout: new Date(),
+        cost: 1200
+      },
+      {
+        roomstype: 2,
+        checkintime: new Date(),
+        checkout: new Date(),
+        cost: 1300
+      }
+    ]
+
   }
 }
